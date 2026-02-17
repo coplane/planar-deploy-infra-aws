bb11815cc04a24a88c8eadbd667e3c39fe7f124b
Ruslan Danilin <ruslan@coplane.com>
2026-02-17 15:29:28 -0800

    Add VPC Flow Logs functionality
    - Introduced resources for VPC Flow Logs, including IAM role, CloudWatch log group, and associated policies.
    - Added variables to enable flow logs and set retention period in days.
    - Enhanced VPC module with logging capabilities for better traffic monitoring.

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -32,6 +32,18 @@ variable "enable_s3_endpoint" {
   default     = true
 }
 
+variable "enable_flow_logs" {
+  description = "Enable VPC Flow Logs"
+  type        = bool
+  default     = true
+}
+
+variable "flow_logs_retention_in_days" {
+  description = "Number of days to retain VPC Flow Logs in CloudWatch"
+  type        = number
+  default     = 7
+}
+
 variable "tags" {
   description = "Additional tags to apply to all resources"
   type        = map(string)
