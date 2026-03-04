6d26a4f3b29c852f9944aafee3a8ba53040cf059
ccnixon <chrisnxn@gmail.com>
2026-03-04 09:18:13 -0600

    Switch to ADOT collector with built-in health check

--- ecs.tf
+++ ecs.tf
@@ -123,7 +123,7 @@ resource "aws_ecs_task_definition" "main" {
     var.telemetry_enabled ? [
       {
         name      = "otel-collector"
-        image     = "otel/opentelemetry-collector-contrib:0.118.0"
+        image     = "public.ecr.aws/aws-observability/aws-otel-collector:v0.43.2"
         essential = true
 
         command = var.log_output_config != null ? [
@@ -149,11 +149,11 @@ resource "aws_ecs_task_definition" "main" {
         )
 
         healthCheck = {
-          command     = ["CMD-SHELL", "wget -q --spider http://localhost:13133/health/status || exit 1"]
+          command     = ["CMD", "/healthcheck"]
           interval    = 30
           timeout     = 5
           retries     = 3
-          startPeriod = 15
+          startPeriod = 30
         }
 
         logConfiguration = {
